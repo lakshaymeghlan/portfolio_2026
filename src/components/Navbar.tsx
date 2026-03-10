cfd1086387679235eca811d8762b5fd4d89174af
Lakshay Meghlan <lakshay.meghlan@techchefz.com>
2026-03-10 23:44:20 +0530

    Migrate from GSAP Trial to standard GSAP (+ Bonus plugins are now free)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
 // @ts-ignore
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Navbar.css";
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 export let smoother: ScrollSmoother;
